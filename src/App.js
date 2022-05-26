0fe9205b44075ad372ccd748ff6eaedba7ee0f33
vaish-0 <98145252+vaish-0@users.noreply.github.com>
2022-05-26 18:38:08 +0530

    Create App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>cahnge is good</code> 
+           <code>change is good</code> 
         </p>
         <a
           className="App-link"
